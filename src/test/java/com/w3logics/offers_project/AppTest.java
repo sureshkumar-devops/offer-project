491f498c5211a0523083c78a1727b152330dc1c6
sureshkumar-devops <suresh.ak47gmail.com>
2024-01-10 15:11:54 +0530

    added initial commit

---
+++ src/test/java/com/w3logics/offers_project/AppTest.java
@@ -0,0 +1,21 @@
+package com.w3logics.offers_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+ 
